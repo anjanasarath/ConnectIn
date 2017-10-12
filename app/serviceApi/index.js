7fa2518bb871d28911a94537b657357967443222
Anjana Sarath <anjanasarath5@gmail.com>
2017-10-12 20:44:16 +0530

    eliminated code redundancy

--- app/serviceApi/index.js
+++ app/serviceApi/index.js
@@ -1,39 +1,31 @@
 const API_URL = "http://posttestserver.com/post.php?dir=example"
 
-export const uploadImageApi = (file) => {
-  var data = new FormData();
-  data.append('file', file);
-
-  return fetch(API_URL, {
-      method: 'POST',
-      body: data
-    }).then((response) => {
-      if (!response.ok) {
-        throw Error(response.statusText);
-      }
-      return response.text();
-    }).then((response) => {
-      console.log(response);
-      return {response}
-    })
-};
-
-export const submitFormApi = (companyName, companyDesc) => {
-  var data = new FormData();
-  data.append('companyName', companyName);
-  data.append('companyDesc', companyDesc)
-
-  return fetch(API_URL,{
+ const fireApiRequest = (API_URL, jData) => {
+    return fetch(API_URL, {
     method: 'POST',
-    body: data
+    body: jData
   }).then((response) => {
-    if(!response.ok) {
+    if (!response.ok) {
       throw Error(response.statusText);
     }
     return response.text();
   }).then((response) => {
     console.log(response);
-
     return {response}
   })
+};
+
+export const uploadImageApi = (file) => {
+  var data = new FormData();
+  data.append('file', file);
+  return fireApiRequest(API_URL, data)
+};
+
+export const submitFormApi = (companyName, companyDesc) => {
+const jdata = {
+    'companyName':companyName,
+    'companyDesc':companyDesc
+  }
+  var data = JSON.stringify(jdata)
+  return fireApiRequest(API_URL,data)
 };
