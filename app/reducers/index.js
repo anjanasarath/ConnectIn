4a2e8bc3006f30d20a305a4e628f6cd134e19cae
Anjana Sarath <anjanasarath5@gmail.com>
2017-11-09 17:44:15 +0530

    Test commoit for LoadingBar

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,10 +1,13 @@
 import { combineReducers } from 'redux';
-import { reducer as formReducer } from 'redux-form'
-import companyFormReducer from './companyFormReducer';
+import { reducer as formReducer } from 'redux-form';
+//import { loadingBarReducer as ldr } from 'react-redux-loading-bar';
+import companyFormReducer  from './companyFormReducer';
+import ldr from './ldr';
 
 const combReducers = combineReducers({
   companyForm: companyFormReducer,
-  form: formReducer
+  form: formReducer,
+  loadingBar: ldr,
 });
 
 export default combReducers;
